1828075ffcf27dd247a26fd1c825aea58fb77b1d
Taylor Brown <taylor.brown.github@gmail.com>
2019-08-31 15:18:26 -0500

    Add tests for players assigned to roles

--- src/main/scala/com/avalon/avalongame/Utils.scala
+++ src/main/scala/com/avalon/avalongame/Utils.scala
@@ -11,108 +11,109 @@ case object TooFewOfPlayers extends RuntimeException with NoStackTrace
 
 object Utils {
 
-  def generateGoodGuysAndBadGuys[F[_]](users: List[User])(implicit F: Sync[F]): F[Players] = {
+  //will have to use a config at some point
+  def generateGoodGuysAndBadGuys[F[_]](users: List[User], shuffle: List[User] => F[List[User]])(implicit F: Sync[F]): F[Players] = {
 
-    import scala.util.control.NoStackTrace
-
-    val shuffled: List[User] = scala.util.Random.shuffle(users)
+//    import scala.util.control.NoStackTrace
+//
+////    val shuffled: List[User] = scala.util.Random.shuffle(users)
 
     val players: StateT[F, List[User], Players] =
       users.size match {
         case 5  =>
           for {
             assassin <- StateT[F, List[User], BadGuy] { s =>
-              F.fromOption(users.headOption.map(u => Assassin(u.nickname)), TooFewOfPlayers).map( (s.drop(1), _) )
+              F.fromOption(s.headOption.map(u => Assassin(u.nickname)), TooFewOfPlayers).map( (s.drop(1), _) )
             }
             normalBadGuy <- StateT[F, List[User], BadGuy] { s =>
-              F.fromOption(users.headOption.map(u => NormalBadGuy(u.nickname)), TooFewOfPlayers).map( (s.drop(1), _) )
+              F.fromOption(s.headOption.map(u => NormalBadGuy(u.nickname)), TooFewOfPlayers).map( (s.drop(1), _) )
             }
             merlin <- StateT[F, List[User], GoodGuy] { s =>
-              F.fromOption(users.headOption.map(u => Merlin(u.nickname)), TooFewOfPlayers).map( (s.drop(1), _) )
+              F.fromOption(s.headOption.map(u => Merlin(u.nickname)), TooFewOfPlayers).map( (s.drop(1), _) )
             }
-            goodGuys <- StateT[F, List[User], List[GoodGuy]] { _ =>
-              F.pure(users.take(2).map(u => NormalGoodGuy(u.nickname))).map( (Nil, _) )
+            goodGuys <- StateT[F, List[User], List[GoodGuy]] { s =>
+              F.pure(s.take(2).map(u => NormalGoodGuy(u.nickname))).map( (Nil, _) )
             }
           } yield Players(List(assassin, normalBadGuy), merlin :: goodGuys)
 
         case 6  =>
           for {
             assassin <- StateT[F, List[User], BadGuy] { s =>
-              F.fromOption(users.headOption.map(u => Assassin(u.nickname)), TooFewOfPlayers).map( (s.drop(1), _) )
+              F.fromOption(s.headOption.map(u => Assassin(u.nickname)), TooFewOfPlayers).map( (s.drop(1), _) )
             }
             normalBadGuy <- StateT[F, List[User], BadGuy] { s =>
-              F.fromOption(users.headOption.map(u => NormalBadGuy(u.nickname)), TooFewOfPlayers).map( (s.drop(1), _) )
+              F.fromOption(s.headOption.map(u => NormalBadGuy(u.nickname)), TooFewOfPlayers).map( (s.drop(1), _) )
             }
             merlin <- StateT[F, List[User], GoodGuy] { s =>
-              F.fromOption(users.headOption.map(u => Merlin(u.nickname)), TooFewOfPlayers).map( (s.drop(1), _) )
+              F.fromOption(s.headOption.map(u => Merlin(u.nickname)), TooFewOfPlayers).map( (s.drop(1), _) )
             }
-            goodGuys <- StateT[F, List[User], List[GoodGuy]] { _ =>
-              F.pure(users.take(3).map(u => NormalGoodGuy(u.nickname))).map( (Nil, _) )
+            goodGuys <- StateT[F, List[User], List[GoodGuy]] { s =>
+              F.pure(s.take(3).map(u => NormalGoodGuy(u.nickname))).map( (Nil, _) )
             }
           } yield Players(List(assassin, normalBadGuy), merlin :: goodGuys)
         case 7  =>
           for {
             assassin <- StateT[F, List[User], BadGuy] { s =>
-              F.fromOption(users.headOption.map(u => Assassin(u.nickname)), TooFewOfPlayers).map( (s.drop(1), _) )
+              F.fromOption(s.headOption.map(u => Assassin(u.nickname)), TooFewOfPlayers).map( (s.drop(1), _) )
             }
             normalBadGuy <- StateT[F, List[User], List[BadGuy]] { s =>
-              F.pure(users.take(2).map(u => NormalBadGuy(u.nickname))).map( (s.drop(1), _) )
+              F.pure(s.take(2).map(u => NormalBadGuy(u.nickname))).map( (s.drop(2), _) )
             }
             merlin <- StateT[F, List[User], GoodGuy] { s =>
-              F.fromOption(users.headOption.map(u => Merlin(u.nickname)), TooFewOfPlayers).map( (s.drop(1), _) )
+              F.fromOption(s.headOption.map(u => Merlin(u.nickname)), TooFewOfPlayers).map( (s.drop(1), _) )
             }
-            goodGuys <- StateT[F, List[User], List[GoodGuy]] { _ =>
-              F.pure(users.take(3).map(u => NormalGoodGuy(u.nickname))).map( (Nil, _) )
+            goodGuys <- StateT[F, List[User], List[GoodGuy]] { s =>
+              F.pure(s.take(3).map(u => NormalGoodGuy(u.nickname))).map( (Nil, _) )
             }
           } yield Players(assassin :: normalBadGuy, merlin :: goodGuys)
         case 8  =>
           for {
             assassin <- StateT[F, List[User], BadGuy] { s =>
-              F.fromOption(users.headOption.map(u => Assassin(u.nickname)), TooFewOfPlayers).map( (s.drop(1), _) )
+              F.fromOption(s.headOption.map(u => Assassin(u.nickname)), TooFewOfPlayers).map( (s.drop(1), _) )
             }
             normalBadGuy <- StateT[F, List[User], List[BadGuy]] { s =>
-              F.pure(users.take(2).map(u => NormalBadGuy(u.nickname))).map( (s.drop(1), _) )
+              F.pure(s.take(2).map(u => NormalBadGuy(u.nickname))).map( (s.drop(2), _) )
             }
             merlin <- StateT[F, List[User], GoodGuy] { s =>
-              F.fromOption(users.headOption.map(u => Merlin(u.nickname)), TooFewOfPlayers).map( (s.drop(1), _) )
+              F.fromOption(s.headOption.map(u => Merlin(u.nickname)), TooFewOfPlayers).map( (s.drop(1), _) )
             }
-            goodGuys <- StateT[F, List[User], List[GoodGuy]] { _ =>
-              F.pure(users.take(4).map(u => NormalGoodGuy(u.nickname))).map( (Nil, _) )
+            goodGuys <- StateT[F, List[User], List[GoodGuy]] { s =>
+              F.pure(s.take(4).map(u => NormalGoodGuy(u.nickname))).map( (Nil, _) )
             }
           } yield Players(assassin :: normalBadGuy, merlin :: goodGuys)
         case 9  =>
           for {
             assassin <- StateT[F, List[User], BadGuy] { s =>
-              F.fromOption(users.headOption.map(u => Assassin(u.nickname)), TooFewOfPlayers).map( (s.drop(1), _) )
+              F.fromOption(s.headOption.map(u => Assassin(u.nickname)), TooFewOfPlayers).map( (s.drop(1), _) )
             }
             normalBadGuy <- StateT[F, List[User], List[BadGuy]] { s =>
-              F.pure(users.take(2).map(u => NormalBadGuy(u.nickname))).map( (s.drop(1), _) )
+              F.pure(s.take(2).map(u => NormalBadGuy(u.nickname))).map( (s.drop(2), _) )
             }
             merlin <- StateT[F, List[User], GoodGuy] { s =>
-              F.fromOption(users.headOption.map(u => Merlin(u.nickname)), TooFewOfPlayers).map( (s.drop(1), _) )
+              F.fromOption(s.headOption.map(u => Merlin(u.nickname)), TooFewOfPlayers).map( (s.drop(1), _) )
             }
-            goodGuys <- StateT[F, List[User], List[GoodGuy]] { _ =>
-              F.pure(users.take(5).map(u => NormalGoodGuy(u.nickname))).map( (Nil, _) )
+            goodGuys <- StateT[F, List[User], List[GoodGuy]] { s =>
+              F.pure(s.take(5).map(u => NormalGoodGuy(u.nickname))).map( (Nil, _) )
             }
           } yield Players(assassin :: normalBadGuy, merlin :: goodGuys)
         case 10 =>
           for {
             assassin <- StateT[F, List[User], BadGuy] { s =>
-              F.fromOption(users.headOption.map(u => Assassin(u.nickname)), TooFewOfPlayers).map( (s.drop(1), _) )
+              F.fromOption(s.headOption.map(u => Assassin(u.nickname)), TooFewOfPlayers).map( (s.drop(1), _) )
             }
             normalBadGuy <- StateT[F, List[User], List[BadGuy]] { s =>
-              F.pure(users.take(3).map(u => NormalBadGuy(u.nickname))).map( (s.drop(1), _) )
+              F.pure(s.take(3).map(u => NormalBadGuy(u.nickname))).map( (s.drop(3), _) )
             }
             merlin <- StateT[F, List[User], GoodGuy] { s =>
-              F.fromOption(users.headOption.map(u => Merlin(u.nickname)), TooFewOfPlayers).map( (s.drop(1), _) )
+              F.fromOption(s.headOption.map(u => Merlin(u.nickname)), TooFewOfPlayers).map( (s.drop(1), _) )
             }
-            goodGuys <- StateT[F, List[User], List[GoodGuy]] { _ =>
-              F.pure(users.take(5).map(u => NormalGoodGuy(u.nickname))).map( (Nil, _) )
+            goodGuys <- StateT[F, List[User], List[GoodGuy]] { s =>
+              F.pure(s.take(5).map(u => NormalGoodGuy(u.nickname))).map( (Nil, _) )
             }
           } yield Players(assassin :: normalBadGuy, merlin :: goodGuys)
       }
 
-    players.runA(shuffled)
+    shuffle(users).flatMap(players.runA)
   }
 
   case class Players(badGuys: List[BadGuy], goodGuys: List[GoodGuy])
---
+++ src/test/scala/com/avalon/avalongame/UtilsSpec.scala
@@ -0,0 +1,191 @@
+package com.avalon.avalongame
+
+import cats.effect.{ContextShift, IO}
+import org.scalatest.{FunSuite, Matchers}
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
+
+class UtilsSpec extends FunSuite with Matchers with ScalaCheckPropertyChecks with enumeratum.ScalacheckInstances {
+  import com.mrdziuban.ScalacheckMagnolia._
+
+  implicit val cs: ContextShift[IO] = IO.contextShift(scala.concurrent.ExecutionContext.Implicits.global)
+
+  test("Should assign 5 players to roles") {
+
+    val taylor = Nickname("Taylor")
+    val chris = Nickname("Chris")
+    val carter = Nickname("Carter")
+    val nick = Nickname("Nick")
+    val cory = Nickname("Cory")
+
+    val users: List[User] =
+      List(
+        User(taylor),
+        User(chris),
+        User(carter),
+        User(nick),
+        User(cory))
+
+    val result = Utils.generateGoodGuysAndBadGuys[IO](users, u => IO.pure(u)).unsafeRunSync()
+
+    (result.badGuys ++ result.goodGuys).size should be(users.size)
+    result.badGuys should contain allOf(Assassin(taylor), NormalBadGuy(chris))
+    result.goodGuys should contain allOf(Merlin(carter), NormalGoodGuy(nick), NormalGoodGuy(cory))
+  }
+
+  test("Should assign 6 players to roles") {
+
+    val taylor = Nickname("Taylor")
+    val chris = Nickname("Chris")
+    val carter = Nickname("Carter")
+    val nick = Nickname("Nick")
+    val cory = Nickname("Cory")
+    val matt = Nickname("Matt")
+
+    val users: List[User] =
+      List(
+        User(taylor),
+        User(chris),
+        User(carter),
+        User(nick),
+        User(cory),
+        User(matt))
+
+    val result = Utils.generateGoodGuysAndBadGuys[IO](users, u => IO.pure(u)).unsafeRunSync()
+
+    (result.badGuys ++ result.goodGuys).size should be(users.size)
+    result.badGuys should contain allOf(Assassin(taylor), NormalBadGuy(chris))
+    result.goodGuys should contain allOf(Merlin(carter), NormalGoodGuy(nick), NormalGoodGuy(cory), NormalGoodGuy(matt))
+  }
+
+  test("Should assign 7 players to roles") {
+
+    val taylor = Nickname("Taylor")
+    val chris = Nickname("Chris")
+    val carter = Nickname("Carter")
+    val nick = Nickname("Nick")
+    val cory = Nickname("Cory")
+    val matt = Nickname("Matt")
+    val liran = Nickname("Liran")
+
+    val users: List[User] =
+      List(
+        User(taylor),
+        User(chris),
+        User(liran),
+        User(carter),
+        User(nick),
+        User(cory),
+        User(matt))
+
+    val result = Utils.generateGoodGuysAndBadGuys[IO](users, u => IO.pure(u)).unsafeRunSync()
+
+    (result.badGuys ++ result.goodGuys).size should be(users.size)
+    result.badGuys should contain allOf(Assassin(taylor), NormalBadGuy(chris), NormalBadGuy(liran))
+    result.goodGuys should contain allOf(Merlin(carter), NormalGoodGuy(nick), NormalGoodGuy(cory), NormalGoodGuy(matt))
+  }
+
+  test("Should assign 8 players to roles") {
+
+    val taylor = Nickname("Taylor")
+    val chris = Nickname("Chris")
+    val carter = Nickname("Carter")
+    val nick = Nickname("Nick")
+    val cory = Nickname("Cory")
+    val matt = Nickname("Matt")
+    val liran = Nickname("Liran")
+    val brian = Nickname("Brian")
+
+    val users: List[User] =
+      List(
+        User(taylor),
+        User(chris),
+        User(liran),
+        User(carter),
+        User(nick),
+        User(cory),
+        User(matt),
+        User(brian))
+
+    val result = Utils.generateGoodGuysAndBadGuys[IO](users, u => IO.pure(u)).unsafeRunSync()
+
+    (result.badGuys ++ result.goodGuys).size should be(users.size)
+    result.badGuys should contain allOf(Assassin(taylor), NormalBadGuy(chris), NormalBadGuy(liran))
+    result.goodGuys should contain allOf(Merlin(carter), NormalGoodGuy(nick), NormalGoodGuy(cory), NormalGoodGuy(matt), NormalGoodGuy(brian))
+  }
+
+  test("Should assign 9 players to roles") {
+
+    val taylor = Nickname("Taylor")
+    val chris = Nickname("Chris")
+    val carter = Nickname("Carter")
+    val nick = Nickname("Nick")
+    val cory = Nickname("Cory")
+    val matt = Nickname("Matt")
+    val liran = Nickname("Liran")
+    val brian = Nickname("Brian")
+    val austin = Nickname("Austin")
+
+    val users: List[User] =
+      List(
+        User(taylor),
+        User(chris),
+        User(liran),
+        User(carter),
+        User(nick),
+        User(cory),
+        User(matt),
+        User(brian),
+        User(austin))
+
+    val result = Utils.generateGoodGuysAndBadGuys[IO](users, u => IO.pure(u)).unsafeRunSync()
+
+    (result.badGuys ++ result.goodGuys).size should be(users.size)
+    result.badGuys should contain allOf(Assassin(taylor), NormalBadGuy(chris), NormalBadGuy(liran))
+    result.goodGuys should contain allOf(
+      Merlin(carter),
+      NormalGoodGuy(nick),
+      NormalGoodGuy(cory),
+      NormalGoodGuy(matt),
+      NormalGoodGuy(brian),
+      NormalGoodGuy(austin))
+  }
+
+  test("Should assign 10 players to roles") {
+
+    val taylor = Nickname("Taylor")
+    val chris = Nickname("Chris")
+    val carter = Nickname("Carter")
+    val nick = Nickname("Nick")
+    val cory = Nickname("Cory")
+    val matt = Nickname("Matt")
+    val liran = Nickname("Liran")
+    val brian = Nickname("Brian")
+    val austin = Nickname("Austin")
+    val justin = Nickname("Justin")
+
+    val users: List[User] =
+      List(
+        User(taylor),
+        User(chris),
+        User(liran),
+        User(justin),
+        User(carter),
+        User(nick),
+        User(cory),
+        User(matt),
+        User(brian),
+        User(austin))
+
+    val result = Utils.generateGoodGuysAndBadGuys[IO](users, u => IO.pure(u)).unsafeRunSync()
+
+    (result.badGuys ++ result.goodGuys).size should be(users.size)
+    result.badGuys should contain allOf(Assassin(taylor), NormalBadGuy(chris), NormalBadGuy(liran), NormalBadGuy(justin))
+    result.goodGuys should contain allOf(
+      Merlin(carter),
+      NormalGoodGuy(nick),
+      NormalGoodGuy(cory),
+      NormalGoodGuy(matt),
+      NormalGoodGuy(brian),
+      NormalGoodGuy(austin))
+  }
+}
