84e84e55cfc4a1c6aaebed65dc6d2e283f440db4
Taylor Brown <kurklazeroth@gmail.com>
2019-11-29 10:42:28 -0600

    Revert "Remove requirement that all players ready up"

--- src/main/scala/com/avalon/avalongame/events/IncomingEvents.scala
+++ src/main/scala/com/avalon/avalongame/events/IncomingEvents.scala
@@ -30,6 +30,8 @@ object Reconnect {
 
 case object StartGame extends IncomingEvent
 
+case object PlayerReady extends IncomingEvent
+
 case class ProposeParty(proposedParty: List[Nickname]) extends IncomingEvent
 object ProposeParty {
   implicit val decoder: Decoder[ProposeParty] = deriveDecoder
@@ -63,6 +65,7 @@ object IncomingEventDecoder {
         case "LeaveGame"            => Right(LeaveGame)
         case "Reconnect"            => Reconnect.decoder.decodeJson(hcursor.value)
         case "StartGame"            => Right(StartGame)
+        case "PlayerReady"          => Right(PlayerReady)
         case "ProposeParty"         => ProposeParty.decoder.decodeJson(hcursor.value)
         case "PartyApprovalVote"    => PartyApprovalVote.decoder.decodeJson(hcursor.value)
         case "QuestVote"            => QuestVoteEvent.decoder.decodeJson(hcursor.value)
--- src/test/scala/com/avalon/avalongame/room/MockRoom.scala
+++ src/test/scala/com/avalon/avalongame/room/MockRoom.scala
@@ -6,7 +6,8 @@ import com.avalon.avalongame.common._
 trait MockRoom extends Room[IO] {
   def players: IO[List[Nickname]] = ???
   def addUser(player: Nickname): IO[Unit] = ???
-  def startGame: IO[StartGameInfo] = ???
+  def startGame: IO[AllPlayerRoles] = ???
+  def playerReady(nickname: Nickname): IO[PlayerReadyEnum] = ???
   def proposeMission(nickname: Nickname, players: List[Nickname]): IO[MissionProposal] = ???
   def teamVote(nickname: Nickname, vote: TeamVote): IO[Either[GameOver, TeamVoteEnum]] = ???
   def questVote(nickname: Nickname, vote: QuestVote): IO[QuestVotingEnum] = ???
