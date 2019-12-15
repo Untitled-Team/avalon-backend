da654b9f5cbc041f073691c41b2a39f20d054eb7
Taylor Brown <taylor.brown.github@gmail.com>
2019-12-14 19:30:56 -0600

    Fix 2 votes required for 4th mission

--- src/main/scala/com/avalon/avalongame/room/Room.scala
+++ src/main/scala/com/avalon/avalongame/room/Room.scala
@@ -193,8 +193,10 @@ object Room {
 
               currentNumberOfAdventurers <- F.fromEither(Missions.fromInt(repr.missions, questPhase.missionNumber)).map(_.numberOfAdventurers)
 
+              currentMissionNumber <- F.fromEither(Missions.currentMission(repr.missions))
+
               currentMissionPassStatus =
-                if (room.players.size >= 7) !(updatedState.votes.map(_.vote).count(_ === QuestVote(false)) >= 2)
+                if (room.players.size >= 7 && currentMissionNumber.number === 4) !(updatedState.votes.map(_.vote).count(_ === QuestVote(false)) >= 2)
                 else !updatedState.votes.map(_.vote).contains(QuestVote(false))
 
               updatedRepr <-
